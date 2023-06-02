127ff56fc4a8f1c1c16a8bc8b2dd2796077d8c94
Carlos Daniel Vargas Varela <carlos.vargasvarela@ucr.ac.cr>
2023-06-01 19:05:27 -0600

    Se compila el proyecto

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/mercado_telas_vue_prueba/'
+  base: '/mercado_telas_vue_prueba/',
 })
